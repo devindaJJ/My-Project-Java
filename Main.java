cd6e86cdcb6da6ff87cb4a4c4390698e145ad356
devindaJJ <it22363534@my.sliit.lk>
2024-01-02 13:05:02 +0530

    Initial Java commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+package DragAndDrop;
+
+public class Main {
+    public static void main(String[] args){
+
+        MyFrame myFrame = new MyFrame();
+
+    }
+}
---
+++ MyFrame.java
@@ -0,0 +1,15 @@
+package DragAndDrop;
+
+import javax.swing.*;
+
+public class MyFrame extends JFrame {
+
+    DragPanel dragPanel = new DragPanel();
+    MyFrame(){
+        this.add(dragPanel);
+        this.setTitle("Drag and Drop");
+        this.setSize(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+    }
+}
